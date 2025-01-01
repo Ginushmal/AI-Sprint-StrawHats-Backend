ba7bd92dd651836fea47cfe66b2d067bfc20d9fd
Isuru Gunarathne <isuru623@gmail.com>
2025-01-01 12:40:34 +0530

    Fix cors

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI,Body ,Request
 from starlette.middleware.sessions import SessionMiddleware
 from app.dependencies import get_session_secret_key, perform_search, perform_search_advanced
@@ -6,17 +7,24 @@ from .chatAgentFuncs import talk_to_gpt
 
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Replace with the frontend's URL
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all HTTP headers
+)
+
 # Add session middleware with secret key
 app.add_middleware(SessionMiddleware, secret_key=get_session_secret_key())
 
 # Include API routes
 app.include_router(router)
 
-
 @app.get("/")
 def read_root():
     return {"Backend": "Healthy"}
-
 
 @app.post("/search")
 def search(query: str):
@@ -27,8 +35,8 @@ def process_json(data: dict = Body(...)):
     return perform_search_advanced(data)
 
 @app.post("/talk_to_bot")
-def talk_to_bot(message: str , request: Request):
-    top_5_search_results = talk_to_gpt(user_input=message,request=request)
+def talk_to_bot(message: str, request: Request):
+    top_5_search_results = talk_to_gpt(user_input=message, request=request)
     return {"top_5_search_results": top_5_search_results}
 
 @app.post("/clear-session")
@@ -37,6 +45,5 @@ async def clear_session(request: Request):
     return {"message": "Session data cleared successfully."}
 
 @app.post("/echo")
-# return the 'message' parameter in the request body
 def echo(message: str):
     return {"message": message}
